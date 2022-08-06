43b9d07b922ef9aa373960f881beca8f7af6219c
Milan Nikic <milnik79@gmail.com>
2022-08-06 20:18:03 +0200

    Add byu book functionality

--- bookstore_test.go
+++ bookstore_test.go
@@ -13,3 +13,40 @@ func TestBook(t *testing.T) {
 		Copies: 2,
 	}
 }
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+	want := 1
+
+	result, err := bookstore.Buy(b)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+
+	if want != got {
+		t.Errorf("want %d copies after buying 1 copy from a stock of 2, got %d", want, got)
+	}
+
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+
+	_, err := bookstore.Buy(b)
+
+	if err == nil {
+		t.Error("want error buying book when zero copies left, but got nil")
+	}
+}
